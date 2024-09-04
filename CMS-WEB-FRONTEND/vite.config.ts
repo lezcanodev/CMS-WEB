3683911f49219218df319423e543b4362082a0d7
AlanFn <alanfabricionunezvera@gmail.com>
2024-09-04 11:51:58 -0400

    vite.config.ts: modifica ruta del proyecto

--- CMS-WEB-FRONTEND/vite.config.ts
+++ CMS-WEB-FRONTEND/vite.config.ts
@@ -5,8 +5,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   //base: 'https://lezcanodev.github.io/CMS-WEB-FRONT-PROD/',
-  base: 'http://localhost:3000/',
+  base: '/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
