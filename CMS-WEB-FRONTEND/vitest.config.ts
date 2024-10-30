38fb777abbbb02b5efaa0098885f40223d356132
FabricioKenobi <fabriciojoel99@gmail.com>
2024-10-30 20:21:52 -0300

    agregar comentarios y notificaciones

--- CMS-WEB-FRONTEND/vitest.config.ts
+++ CMS-WEB-FRONTEND/vitest.config.ts
@@ -1,9 +1,10 @@
 /// <reference types="vitest/config" />
-
+import react from '@vitejs/plugin-react'
 import path from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  plugins:[react()],
   test: {
     environment: 'jsdom',
     setupFiles: ['./vitest.setup.ts'],
