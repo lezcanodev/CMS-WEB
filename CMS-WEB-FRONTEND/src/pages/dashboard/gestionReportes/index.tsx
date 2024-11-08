1a9ae41a6e40de79158b628b51ef1426d4b381aa
Lex4r <axelfacundonunezvera@gmail.com>
2024-11-07 22:18:27 -0300

    Refactoriza la seccion reportes

--- CMS-WEB-FRONTEND/src/pages/dashboard/gestionReportes/index.tsx
+++ CMS-WEB-FRONTEND/src/pages/dashboard/gestionReportes/index.tsx
@@ -19,46 +19,8 @@ export default function GestionUsuarios(){
     const { data: libros, loading: loadingLibros } = useAppSelector((state) => state.api.libro.listar);
     const [libro_filtrado, setLibros]  = useState(libros?.data || []);
     const [reload, setReload] = useState<boolean>(false);
-
-    // para obtener todos los datos y luego cargar en la tabla
-    useEffect(() => {
-       dispatch(api.libro.libroListarApiThunk());
-    },[dispatch,reload]);
-
-    // useEffect para filtrar libros recién agregados
-    useEffect(() => {
-        const recienAgregados = () => {
-            const mesActual = new Date().getMonth()+1;
-            const librosFiltrados = libros?.data?.filter(libro => {
-                return BigInt(libro.fecha.split('/')[1]) === BigInt(mesActual);
-            }) || [];
-            return librosFiltrados;
-        };
-        setLibros(recienAgregados());
-    }, [libros]);
-
-
-    const rank_visitados = <Ranking
-    titulo = 'Los mas visitados'
-    libros = {libros?.data || []}
-    />;
-
-    const rank_gustados = <Ranking
-    titulo = 'Los mas gustados'
-    libros = {libros?.data || []}
-     />;
-
-    const rank_ingresados = <Ranking
-    titulo = 'Agregados recientemente'
-    libros = {libro_filtrado}
-    />;
     
     return<>
         <CategoriasConMasLibros />
-        {rank_gustados}
-        {rank_ingresados}
-        {rank_visitados}
-
-        
     </>
 }
