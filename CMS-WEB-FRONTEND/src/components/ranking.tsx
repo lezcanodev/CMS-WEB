da05ea58364a44d765c2260cab1885d49535ecf0
FabricioKenobi <fabriciojoel99@gmail.com>
2024-11-07 23:10:10 -0300

    cambios para pruebas de sonar

--- CMS-WEB-FRONTEND/src/components/ranking.tsx
+++ CMS-WEB-FRONTEND/src/components/ranking.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Slider from "react-slick";
-import { Box, Paper, Stack, Typography } from '@mui/material';
+import {  Typography } from '@mui/material';
 import { LibroListarData } from '@/api/gestionLibros/listar/listarLibro.model';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
