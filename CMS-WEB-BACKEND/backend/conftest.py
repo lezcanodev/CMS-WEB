3b8458076df40c7ec88079cb03e2e838036ffb21
FabricioKenobi <fabriciojoel99@gmail.com>
2024-11-15 16:01:05 -0300

    test de http

--- CMS-WEB-BACKEND/backend/conftest.py
+++ CMS-WEB-BACKEND/backend/conftest.py
@@ -4,10 +4,7 @@ import os
 import django
 from django.conf import settings
 
-@pytest.fixture(scope='session')
-def django_db_setup(django_db_blocker):
-    with django_db_blocker.unblock():
-        call_command('migrate', run_syncdb=True)
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 django.setup()
--- CMS-WEB-BACKEND/backend/test/test_http.py
+++ CMS-WEB-BACKEND/backend/test/test_http.py
@@ -5,6 +5,92 @@ from rest_framework import status
 from api.models import Histograma, User, Libro,Categoria
 
 @pytest.mark.django_db
+def test_listar_usuarios_autenticado():
+    # Configuración del cliente de prueba
+    client = APIClient()
+
+    # Crear un usuario autenticado
+    usuario = User.objects.create_user(username="testuser", password="testpass")
+    client.force_authenticate(user=usuario)
+
+    # Obtener la URL de la vista
+    url = reverse("listar-usuario")  # Cambia esto por el nombre real en tu urls.py
+
+    # Hacer una solicitud GET a la vista
+    response = client.get(url)
+
+    # Verificar que el código de respuesta sea 200
+    assert response.status_code == status.HTTP_200_OK
+
+    # Verificar que los datos sean correctos
+    resultados = response.json()
+    assert len(resultados) == 1
+    assert resultados[0]["role"] == 'Suscriptor'
+    
+
+
+@pytest.mark.django_db
+def test_listar_usuarios_no_autenticado():
+    # Configuración del cliente de prueba
+    client = APIClient()
+
+    # Obtener la URL de la vista
+    url = reverse("listar-usuario")  # Cambia esto por el nombre real en tu urls.py
+
+    # Hacer una solicitud GET sin autenticación
+    response = client.get(url)
+
+    # Verificar que el código de respuesta sea 401 (No autorizado)
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    
+
+#test de listar usuarios
+@pytest.mark.django_db
+def test_listar_categoria():
+    
+    x = Categoria.objects.count()
+    client = APIClient()
+    
+    #se crea una categoria de prueba
+    categoria = Categoria.objects.create(nombre="Ciencia Ficción")
+    otra_categoria = Categoria.objects.create(nombre="Accion")
+    
+    url= reverse('listar-categoria')
+    response = client.get(url,{'categoria':categoria.id})
+    
+    
+    assert response.status_code == status.HTTP_200_OK
+    
+    resultados = response.json()
+    
+    assert len(resultados) == x+2
+    assert resultados[x]['nombre'] == categoria.nombre
+    assert resultados[x+1]['nombre'] == otra_categoria.nombre
+
+
+#test de listar libros
+@pytest.mark.django_db
+def test_listar_libros():
+    x = Libro.objects.count()
+    client = APIClient()
+    
+    usuario = User.objects.create(username="testuser", password="testpass")
+    categoria = Categoria.objects.create(nombre="Ciencia Ficción")
+    libro = Libro.objects.create(titulo="El viaje espacial", author=usuario, categoria=categoria, contenido="Contenido del libro",likes=0,vistas=0)
+    
+    url = reverse('listar-libro')
+    response = client.get(url,{'libro':libro.id})
+    
+    assert response.status_code == status.HTTP_200_OK
+    
+    resultados = response.json()
+    
+    assert len(resultados) == x+1
+    assert  resultados[x]['titulo']  == libro.titulo
+    
+    
+#test de listar historial de acciones
+@pytest.mark.django_db
 def test_histograma_listar_filtrado_por_libro():
     # Configuración de cliente y datos
     client = APIClient()
